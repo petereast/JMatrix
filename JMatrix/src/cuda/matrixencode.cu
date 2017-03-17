3799dde7d1dec3402cc3745b5da13e92ac2372be
Peter East <08peast@gmail.com>
2017-03-17 09:46:24 +0000

    Adding some cuda stuff slowly

---
+++ JMatrix/src/cuda/matrixencode.cu
@@ -0,0 +1,14 @@
+//c++
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+	printf("Hello world\n");
+
+	// Read matrices metadata from stdin
+	// Allocate memory (both on host & device)
+	// read the matrix data from stdin
+	// perform accelerated matrix calculation
+	// write output (& metadata) to stdout
+	return 0;
+}
---
+++ JMatrix/src/org/JMatrix/CuMatrix.java
@@ -0,0 +1,22 @@
+package org.JMatrix;
+
+public class CuMatrix extends matrix {
+	
+	// Accelerated matrix methods
+	// 		Each operation is forked out to a CUDA binary - which means we need
+	//		a really nice way of encoding matrices and operations into strings
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Creating an accelerated matrix");
+
+	}
+	
+	public void iTest(){
+		System.out.println(this.data);
+	}
+
+}
